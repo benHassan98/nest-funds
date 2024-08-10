177d4effc8d8450cb7a459c016deaa5c7aadbb40
Abdullah Mahmoud <abdullahH.Mahmoud98@gmail.com>
2024-08-10 15:29:21 +0000

    database chapter

---
+++ src/songs/dto/create-song.dto.ts
@@ -0,0 +1,22 @@
+import { IsArray, IsDateString, IsMilitaryTime, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateSongDto {
+
+    @IsString()
+    @IsNotEmpty()
+    readonly title:string;
+
+    @IsNotEmpty()
+    @IsArray()
+    @IsString({ each: true })
+    readonly artists: string[];
+
+    @IsDateString()
+    @IsNotEmpty()
+    readonly releasedDate: Date;
+
+    @IsMilitaryTime()
+    @IsNotEmpty()
+    readonly duration: Date;
+
+}
